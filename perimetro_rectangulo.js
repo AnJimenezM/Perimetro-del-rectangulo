c0e4683ab0effe9b21a69f6ea5bdcd4ef6145cfe
AnJimenezM <136032517+AnJimenezM@users.noreply.github.com>
2023-06-24 12:38:31 -0500

    commit

---
+++ perimetro_rectangulo.js
@@ -0,0 +1,9 @@
+
+
+var base= parseInt(prompt("ingresar base"));
+      var altura = parseInt(prompt("ingresar altura"));
+      
+
+      perimetro = 2 * base + 2 * altura;
+
+      alert("el perimetro del rectangulo es" + perimetro);
